cf36d4bbab0545f274b7607499c1ad93ec9d6463
ST10306742 <st10306742@vcconnect.edu.za>
2025-09-27 20:20:42 +0200

    Changed message in new branch

--- app/src/main/java/com/example/multibranchexample/MainActivity.kt
+++ app/src/main/java/com/example/multibranchexample/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MultiBranchExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android from the new branch!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
